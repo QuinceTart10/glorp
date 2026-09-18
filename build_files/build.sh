ece86c37d453ed17f4e532801fff7fb005b5a9c4
quince <quince@privacyslop.zip>
2026-09-18 14:03:53 +0200

    remove fedora's browser defaults and plasma-browser-integration here's to hoping nothing blows up also reorganizes build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,15 +2,28 @@
 
 set -ouex pipefail
 
-# libvirtery
-dnf5 install -y qemu libvirt guestfs-tools
-systemctl enable libvirtd.service
+# stripdownery
+dnf5 remove -y xwaylandvideobridge fedora-bookmarks fedora-chromium-config fedora-chromium-config-kde plasma-browser-integration
 
 # automountery
 systemctl disable ublue-os-media-automount.service
 dnf5 remove -y ublue-os-media-automount-udev
 rm /usr/lib/udev/rules.d/99-framework-steam-automount.rules
 rm /usr/lib/udev/rules.d/99-steamos-automount.rules
+
+# ntfsery
+systemctl --global disable ntfs-nag.service
+rm /usr/lib/systemd/user/ntfs-nag.service
+rm /usr/libexec/ntfs-exfat-monitor-script
+
+# bbrewery
+rm /usr/bin/bbrew-helper
+rm /usr/share/applications/bbrew.desktop
+rm /usr/share/ublue-os/bbrew.png
+
+# libvirtery
+dnf5 install -y qemu libvirt guestfs-tools
+systemctl enable libvirtd.service
 
 # user and group fix, taken from bazzite
 if [ -f /etc/passwd ]; then
@@ -34,19 +47,6 @@ if [ -f /etc/group ]; then
         echo "wheel:x:10:" >> /etc/group
     fi
 fi
-
-# ntfsery
-systemctl --global disable ntfs-nag.service
-rm /usr/lib/systemd/user/ntfs-nag.service
-rm /usr/libexec/ntfs-exfat-monitor-script
-
-# bbrewery
-rm /usr/bin/bbrew-helper
-rm /usr/share/applications/bbrew.desktop
-rm /usr/share/ublue-os/bbrew.png
-
-# xwaylandvideobridgery
-dnf5 remove -y xwaylandvideobridge
 
 # tailscalery
 systemctl enable tailscaled.service
